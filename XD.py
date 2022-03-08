e14a6279044882cfa312f5986ee3a2ec16f7aa8e
SEWhuskylover <hoskingm2@middleton.school.nz>
2022-03-09 10:09:46 +1300

    thing

--- XD.py
+++ XD.py
@@ -1,11 +1,31 @@
 yesno = 2
-while yesno != 1:
-    
-
-
+total_adult = 0
+total_child = 0
+total_student = 0
+total_gift = 0
+profit = 0
+while yesno == 2:
+    ticket = input("what ticket do you want\n 1 for adult\n 2 for student\n 3 for child\n 4 for gift voucher: \n")
+    if ticket == 1:
+        print("sold")
+        total_adult = total_adult + 1
+        profit = profit + 12.5
+    if ticket == 2:
+        print("sold")
+        total_student = total_student + 1
+        profit = profit + 9
+    if ticket == 3:
+        print("sold")
+        total_child = total_child + 1
+        profit = profit + 7
+    if ticket == 4:
+        print("thankyou")
+        total_gift = total_gift + 1
     yesno = input("do you want to sell another 2 for yes 1 for no:\n")
-    while yesno != 1 or yesno != 2:
-    yesno = input("do you want to sell another 2 for yes 1 for no:\n")
-    print("please enter a valid answer")
-
+print("summary for today")
+print(f"total adult tickets sold \n = {total_adult}")
+print(f"total student tickets sold \n = {total_student}")
+print(f"total child tickets sold \n = {total_child}")
+print(f"total gift cards \n {total_gift}")
+print(f"total profit \n {profit}")
 
